f613b0274978b96c1f5589506a798c0b9d8de9c6
Rick <r.banks@burnley.ac.uk>
2019-08-12 11:19:58 +0100

    Add SimpleMovement Scripts

---
+++ CoRoutines/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveTime = 3f;
+
+    private Vector3 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SimpleMove());
+        startPos = transform.position;
+    }
+
+    IEnumerator SimpleMove()
+    {
+        float t = 0f;
+
+        while (t < moveTime)
+        {
+            t += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(startPos, target.position, t / moveTime);
+
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+}
---
+++ CoRoutines/Assets/Scripts/SimplePatrol.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePatrol : MonoBehaviour {
+
+    public Transform target_01;
+    public Transform target_02;
+
+    private Vector3 startPos;
+    public float moveTime;
+
+	private void Start()
+	{
+        startPos = transform.position;
+        StartCoroutine(MoveToTarget_01());
+	}
+
+
+    // Patrol
+
+    IEnumerator MoveToTarget_01()
+    {
+        float t = 0f;
+
+        while (t < moveTime)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, target_01.position, t / moveTime);
+            yield return new WaitForEndOfFrame();
+        }
+        startPos = transform.position;
+        StartCoroutine(MoveTotarget_02());
+    }
+
+    IEnumerator MoveTotarget_02()
+    {
+        float t = 0;
+        while(t < moveTime)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, target_02.position, t / moveTime);
+            yield return new WaitForEndOfFrame();
+        }
+        startPos = transform.position;
+        StartCoroutine(MoveToTarget_01());
+    }
+
+    // End Patrol
+}
